ce8e8421929236369801406205ac8f3f370aed75
Gerreqiem <gerreqiem@gmail.com>
2026-05-15 11:01:11 +0300

    Глобальный обработчик исключений

--- TechInventory/App.xaml.cs
+++ TechInventory/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using TechInventory.Helpers;
 
 namespace TechInventory
 {
@@ -7,6 +8,15 @@ namespace TechInventory
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            DispatcherUnhandledException += (s, ex) =>
+            {
+                Logger.Log("Необработанное исключение: " + ex.Exception.ToString());
+                MessageBox.Show($"Произошла ошибка: {ex.Exception.Message}", "Ошибка",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                ex.Handled = true;
+            };
+
             var loginWindow = new Views.LoginWindow();
             loginWindow.Show();
         }
